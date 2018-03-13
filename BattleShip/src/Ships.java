68c5230f41e86eb5882fed2abaa28baedd6bf025
2000JR <32545968+2000JR@users.noreply.github.com>
2018-03-13 09:53:32 -0400

    Add files via upload

--- BattleShip/src/Ships.java
+++ BattleShip/src/Ships.java
@@ -1,9 +1,41 @@
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Ships {
-
+	Scanner in = new Scanner(System.in);
+	static private String shipname;
+	static private int shiplength;
+	static private int shiphealth;
+	static int[][] shipposition;
+	static String hit;
+	//private Arraylist<> Positioning; possibly for positioning
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Welcome to the shape builder!");
+		System.out.println("How many ships do you want?");
+		//Scanner namer = new Scanner(System.in);
+		//Scanner measurements = new Scanner(System.in);
+		
 	}
 
+
+
+
+	public static void shipbuilder(String name, int length, int health, int[][] position) {
+		
+		shipname = name;
+		shiplength = length;
+		shiphealth = health;
+		shipposition = position;
+			
+			
+			
+
+
+	}
+	public static void damage() {
+		
+	}
 }
+
